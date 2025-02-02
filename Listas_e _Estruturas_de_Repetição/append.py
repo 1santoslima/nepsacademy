2b52d07f0256a98582e0d84f28df9bb9d08728dc
Igor <igorlimaisl7@gmail.com>
2025-02-02 16:11:00 -0300

    lista

---
+++ Listas_e _Estruturas_de_Repetição/append.py
@@ -0,0 +1,8 @@
+list = [1,2,3,4,5]
+list.append(6)
+print(list)
+print(list[0])
+print(list[2])
+print(list[5],list[4])
+print(list[5])
+print(list[5])
---
+++ Listas_e _Estruturas_de_Repetição/tiposdedados.py
@@ -0,0 +1,2 @@
+lista = [1,2,3,True , "Igor",1.0]
+print(type(list))
